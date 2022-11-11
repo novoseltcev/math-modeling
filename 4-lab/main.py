e98751db4f540a2387d8abb585269eab16dd3b1a
novoseltcev <novoseltcev.stanislav@gmail.com>
2022-11-11 13:22:19 +0300

    fix(4-lab): Invalid sping_y

--- 4-lab/main.py
+++ 4-lab/main.py
@@ -3,13 +3,14 @@ import numpy as np
 
 
 def calc(T1: float, T2: float, r: float, omega: float) -> None:
-
+    y0 = 0
+    phi0 = 0
+    g = 9.8
     t = np.linspace(0, T1, 1000)
 
-    spring_y = -10 * np.cos(omega * t)
-
-    rx = r * np.cos(((2 * np.pi) / (T1)) * t)
-    ry = r * np.sin(((2 * np.pi) / (T1)) * t) + spring_y
+    spring_y = (y0 + g / omega ** 2) * np.cos(omega * t) - g / omega ** 2
+    rx = r * np.cos(phi0 + 2 * np.pi * t / T1)
+    ry = r * np.sin(phi0 + 2 * np.pi * t / T1) + spring_y
 
     plt.figure()
     plt.plot(rx, ry)
